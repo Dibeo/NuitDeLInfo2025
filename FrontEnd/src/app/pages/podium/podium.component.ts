5fc00e743c26815afd11f71690376613164b6d12
JVeluz <j.veluz2002@gmail.com>
2025-12-04 22:46:00 +0100

    Podium Page

--- FrontEnd/src/app/pages/podium/podium.component.ts
+++ FrontEnd/src/app/pages/podium/podium.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import UserProgressRepository from '../../../core/UserProgressAPI/UserProgressRepository';
 
 @Component({
   selector: 'app-podium',
   imports: [],
   templateUrl: './podium.component.html',
-  styleUrl: './podium.component.css'
+  styleUrl: './podium.component.css',
 })
 export class PodiumComponent {
+  public currentXp: number = 0;
 
+  public async addXp(): Promise<void> {
+    this.currentXp = await UserProgressRepository.addXp(1);
+  }
+
+  public async getXp(): Promise<void> {
+    this.currentXp = await UserProgressRepository.getXp();
+    console.log(await UserProgressRepository.getAll());
+  }
 }

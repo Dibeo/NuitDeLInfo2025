6b71184b7b67a66545c90faa10d922b4f30fc743
JVeluz <j.veluz2002@gmail.com>
2025-12-04 19:25:57 +0100

    Podium Page Added
    Page + Route

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PodiumComponent } from './pages/podium/podium.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'podium', component: PodiumComponent }
+];

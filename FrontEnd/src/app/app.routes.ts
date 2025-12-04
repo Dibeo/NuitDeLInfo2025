0e0b4f88d7b3f03d808f99f969dd38320f94d049
Titouan Charrier <titouan@charrier.ovh>
2025-12-04 20:39:56 +0100

    copie du composant tree

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainVisualizer } from './component/VisualisationAudio/main-visualizer/main-visualizer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"VisualisationAudio", component : MainVisualizer}
+];

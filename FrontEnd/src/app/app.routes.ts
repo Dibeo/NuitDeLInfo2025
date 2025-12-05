7bdff51330d1ff5488502af9bc433e46a93e8704
Simon <115413042+Dibeo@users.noreply.github.com>
2025-12-05 07:01:05 +0100

    Refactor routing and HTML structure; remove netlify.toml configuration

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { MainVisualizer } from './component/VisualisationAudio/main-visualizer/main-visualizer'; 
+import { MainVisualizer } from './component/VisualisationAudio/main-visualizer/main-visualizer';
 import { PodiumComponent } from './pages/podium/podium.component';
 import { Scene } from './components/Principal/scene/scene';
 
 export const routes: Routes = [
-    { path: 'podium', component : PodiumComponent },
-    { path:'' , component : Scene },
-    {path:"visualisation-audio", component : MainVisualizer}
+  { path: 'podium', component: PodiumComponent },
+  { path: '', component: Scene },
+  { path: 'visualisation-audio', component: MainVisualizer },
 ];
